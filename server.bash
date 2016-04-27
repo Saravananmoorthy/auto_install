206d1e304c587d9103c700ce509578fd35090b87
uevol <uevol@outlook.com>
2016-04-27 22:20:51 +0800

    setup iptables

--- server.bash
+++ server.bash
@@ -4,17 +4,23 @@ set -e
 
 printf "=====================================\n"
 printf "starting to install and please wait  \n"
-printf "=====================================\n"
+printf "=====================================\n\n\n\n\n\n\n\n"
 
 
 PUPPET_BIN_DIR="/usr/bin/"
 PUPPET_SERVER="`hostname --fqdn`"
 PUPPET_SERVER_IP="`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`"
 
+printf "=====================================\n"
+printf "service iptalbes stop ..........  \n"
+printf "=====================================\n\n\n\n\n\n\n\n\n"
+
+service iptalbes stop
+chkconfig iptalbes off
 
 printf "=====================================\n"
 printf "add master to /etc/hosts ..........  \n"
-printf "=====================================\n"
+printf "=====================================\n\n\n\n\n\n\n\n\n"
 
 if ! grep "${PUPPET_SERVER}" /etc/hosts; then
   echo "${PUPPET_SERVER_IP}     ${PUPPET_SERVER}" >> /etc/hosts 
@@ -22,7 +28,7 @@ fi
 
 printf "=====================================\n"
 printf "add native yum for this computer.......\n"
-printf "=====================================\n"
+printf "=====================================\n\n\n\n\n\n\n\n\n"
 
 mkdir -p /etc/yum.repos.d/bak
 cd /etc/yum.repos.d/
@@ -55,19 +61,19 @@ fi
 
 printf "=====================================\n"
 printf "start to install foreman........ \n"
-printf "=====================================\n"
+printf "=====================================\n\n\n\n\n\n\n\n\n"
 
 yum -y install epel-release 
 yum -y install foreman-installer
 foreman-installer
 
 
-printf "=========================================\n"
-printf "use default system  yum for this computer\n"
-printf "=========================================\n"
+# printf "=========================================\n"
+# printf "use default system  yum for this computer\n"
+# printf "=========================================\n"
 
-cd /etc/yum.repos.d/bak
-mv *.repo /etc/yum.repos.d/
-mv /etc/yum.repos.d/puppetlabs.repo /etc/yum.repos.d/bak
+# cd /etc/yum.repos.d/bak
+# mv *.repo /etc/yum.repos.d/
+# mv /etc/yum.repos.d/puppetlabs.repo /etc/yum.repos.d/bak
 
 exit 0
